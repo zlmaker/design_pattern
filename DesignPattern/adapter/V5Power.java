24a351a74d0edbad5069fdceb87dbd2cab900137
zlmaker <115923725+zlmaker@users.noreply.github.com>
2022-10-18 17:11:07 +0800

    适配模式

---
+++ DesignPattern/adapter/V5Power.java
@@ -0,0 +1,5 @@
+package adapter;
+
+public interface V5Power {
+    int provideV5Power();
+}
---
+++ DesignPattern/adapter/V5PowerAdapter.java
@@ -0,0 +1,17 @@
+package adapter;
+
+public class V5PowerAdapter implements V5Power {
+    private V220Power v220Power;
+
+    public V5PowerAdapter(V220Power v220Power) {
+        this.v220Power = v220Power;
+    }
+
+    @Override
+    public int provideV5Power() {
+        int power = v220Power.provideV220Power();
+        //power经过各种操作-->5
+        System.out.println("适配器：当前电压为" + power + "，我悄悄的适配了电压，将其转为5v了");
+        return 5;
+    }
+}
