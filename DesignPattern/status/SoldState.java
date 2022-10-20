af3c13a12f0b1b1fccb0827109a7108be6524138
zlmaker <115923725+zlmaker@users.noreply.github.com>
2022-10-21 00:47:06 +0800

    状态模式

---
+++ DesignPattern/status/SoldState.java
@@ -0,0 +1,35 @@
+package status;
+
+public class SoldState implements State {
+    private VendingMachine machine;
+
+    public SoldState(VendingMachine machine) {
+        this.machine = machine;
+    }
+
+    @Override
+    public void insertMoney() {
+        System.out.println("正在出货，请勿投币");
+    }
+
+    @Override
+    public void backMoney() {
+        System.out.println("正在出货，没有可退的钱");
+    }
+
+    @Override
+    public void turnCrank() {
+        System.out.println("正在出货，请勿重复转动手柄");
+    }
+
+    @Override
+    public void dispense() {
+        machine.dispense();
+        if (machine.getCount() > 0) {
+            machine.setCurrentState(machine.getNoMoneyState());
+        } else {
+            System.out.println("商品已经售罄");
+            machine.setCurrentState(machine.getSoldOutState());
+        }
+    }
+}
---
+++ DesignPattern/status/VendingMachine.java
@@ -0,0 +1,93 @@
+package status;
+
+
+/**
+ * 自动售货机
+ */
+public class VendingMachine {
+    private State noMoneyState;
+    private State hasMoneyState;
+    private State soldState;
+    private State soldOutState;
+    private State currentState = null;
+    private State winnerState;
+    private int count = 0;
+
+    public VendingMachine(int count) {
+        noMoneyState = new NoMoneyState(this);
+        hasMoneyState = new HasMoneyState(this);
+        soldState = new SoldState(this);
+        soldOutState = new SoldOutState(this);
+        winnerState = new WinnerState(this);
+
+        if (count > 0) {
+            this.count = count;
+            currentState = noMoneyState;
+        }
+    }
+
+    public int getCount() {
+        return count;
+    }
+
+    public void setCurrentState(State state) {
+        this.currentState = state;
+    }
+
+    public State getNoMoneyState() {
+        return noMoneyState;
+    }
+
+    public VendingMachine setNoMoneyState(State noMoneyState) {
+        this.noMoneyState = noMoneyState;
+        return this;
+    }
+
+    public State getSoldState() {
+        return soldState;
+    }
+
+    public VendingMachine setSoldState(State soldState) {
+        this.soldState = soldState;
+        return this;
+    }
+
+    public State getSoldOutState() {
+        return soldOutState;
+    }
+
+    public VendingMachine setSoldOutState(State soldOutState) {
+        this.soldOutState = soldOutState;
+        return this;
+    }
+
+    public State getWinnerState() {
+        return winnerState;
+    }
+
+    public VendingMachine setWinnerState(State winnerState) {
+        this.winnerState = winnerState;
+        return this;
+    }
+
+    public void insertMoney() {
+        currentState.insertMoney();
+    }
+
+    public void backMoney() {
+        currentState.backMoney();
+    }
+
+    public void turnCrank() {
+        currentState.turnCrank();
+        if (currentState == soldState || currentState == winnerState)
+            currentState.dispense();
+    }
+
+    public void dispense() {
+        System.out.println("发出一件商品...");
+        if (count != 0) {
+            count -= 1;
+        }
+    }
+}
