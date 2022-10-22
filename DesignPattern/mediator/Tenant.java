bfacf24f4646cde004a42396c7f4f26b09df9464
zlmaker <115923725+zlmaker@users.noreply.github.com>
2022-10-22 17:25:21 +0800

    中介者模式

---
+++ DesignPattern/mediator/Tenant.java
@@ -0,0 +1,12 @@
+package mediator;
+
+public class Tenant extends Person {
+    public Tenant(String name, Mediator mediator) {
+        super(name, mediator);
+    }
+
+    @Override
+    public void getMessage(String message) {
+        System.out.println("租房者" + name + "获取到的信息：" + message);
+    }
+}
