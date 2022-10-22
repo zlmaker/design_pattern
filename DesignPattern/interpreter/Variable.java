dcf82760614a16e90b5ea592f9938b6d8ab148e3
zlmaker <115923725+zlmaker@users.noreply.github.com>
2022-10-23 00:47:07 +0800

    解释器模式

---
+++ DesignPattern/interpreter/Variable.java
@@ -0,0 +1,19 @@
+package interpreter;
+
+public class Variable implements Expression {
+    private String name;
+
+    public Variable(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public int interpret(Context context) {
+        return context.getValue(this);
+    }
+
+    @Override
+    public String toString() {
+        return name;
+    }
+}
